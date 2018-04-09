9e61eba5ee98977eb52ca5a587be03afed89e0c2
Paul Schwin <schwin42@gmail.com>
2018-04-08 22:44:09 -0700

    Worked around issue with trackers being disabled on start

---
+++ Assets/DojoGameMaster.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DojoGameMaster : MonoBehaviour {
+
+	public GameObject[] trackers;
+
+	// Use this for initialization
+	void Awake () {
+		//HACK Something is disabling the trackers, reenable them manually
+		trackers = GameObject.FindGameObjectsWithTag("Tracker");
+		StartCoroutine(EnableTrackers());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	IEnumerator EnableTrackers() {
+		print("start");
+		yield return new WaitForSeconds(0.1F);
+		print("enabling trackers");
+		foreach (GameObject go in trackers) {
+			print("enable");
+			go.SetActive(true);
+		}
+	}
+}
