4bd13f84d285239243df0c8259f594ef1a143634
Paul Schwin <schwin42@gmail.com>
2018-04-08 18:24:55 -0700

    Implementation of foot tracking and collision

---
+++ Assets/DestructibleProps/BulletsDamage.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletsDamage : MonoBehaviour {
+	[Range(1,100)]
+	public int Damage = 34;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void OnCollisionEnter(Collision other){
+		print("Collision enter");
+//		Destroy (gameObject);
+	}
+}
---
+++ Assets/Target.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour {
+
+	int weaponLayer;
+
+	// Use this for initialization
+	void Start () {
+		weaponLayer = LayerMask.NameToLayer("Weapon");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnCollisionEnter(Collision other){
+		if (other.gameObject.layer == weaponLayer) {
+			print("Collision enter");
+			//		Destroy (gameObject);
+			Destroy(gameObject);
+		}
+	}
+}
