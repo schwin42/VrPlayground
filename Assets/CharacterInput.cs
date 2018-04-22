79578e57b127dde831fe2e5fad904bc07a2d9b20
Paul Schwin <schwin42@gmail.com>
2018-04-21 20:37:50 -0700

    Basic character navigation

---
+++ Assets/CharacterInput.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class CharacterInput : MonoBehaviour {
+
+	NavMeshAgent agent;
+
+	// Use this for initialization
+	void Start () {
+		agent = GetComponent<NavMeshAgent>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetMouseButtonUp(0)) {
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			RaycastHit hit;
+			if(Physics.Raycast(ray, out hit)) {
+				agent.SetDestination(hit.point);
+			}
+		}
+	}
+}
