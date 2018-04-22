ba46ef3ceb69b14c68c52ba3232d45551d774c51
Paul Schwin <schwin42@gmail.com>
2018-04-21 22:25:23 -0700

    Progress towards incorporating animation into movement

--- Assets/CharacterInput.cs
+++ Assets/CharacterInput.cs
@@ -2,15 +2,32 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityStandardAssets.Characters.ThirdPerson;
 
 [RequireComponent(typeof(NavMeshAgent))]
+[RequireComponent(typeof(ThirdPersonCharacter))]
 public class CharacterInput : MonoBehaviour {
 
 	NavMeshAgent agent;
 
+	public ThirdPersonCharacter character;
+
+
+
 	// Use this for initialization
 	void Start () {
 		agent = GetComponent<NavMeshAgent>();
+		character = GetComponent<ThirdPersonCharacter>();
+
+		Component[] components = GetComponents(typeof(Component));
+		foreach (Component component in components) {
+			print("Component: " + component.GetType().ToString());
+		}
+
+		print("character: " + (GetComponent<UnityStandardAssets.Characters.ThirdPerson.ThirdPersonCharacter>() == null ? "null" : "thing"));
+
+
+		agent.updateRotation = false;
 	}
 	
 	// Update is called once per frame
@@ -22,5 +39,11 @@ public class CharacterInput : MonoBehaviour {
 				agent.SetDestination(hit.point);
 			}
 		}
+
+//		if (agent.remainingDistance > agent.stoppingDistance) {
+//			character.Move(agent.desiredVelocity, false, false);
+//		} else {
+//			character.Move(Vector3.zero, false, false);
+//		}
 	}
 }
